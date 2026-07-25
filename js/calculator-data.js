655cd16ca69c3d58caafac3ef4244a07ecb1b581
laheyengineering-lab <laheyengineering@gmail.com>
2026-07-25 17:00:16 -0400

    forgot calculator-data.js script

---
+++ js/calculator-data.js
@@ -0,0 +1,22 @@
+const calculators = [
+    {
+        name: "Moment Calculator",
+        category: "Mechanical",
+        description: "Calculate torque from force and distance.",
+        link: "calculators/moment.html"
+    },
+
+    {
+        name: "Bolt Torque Calculator",
+        category: "Fasteners",
+        description: "Coming Soon",
+        link: "#"
+    },
+
+    {
+        name: "Stress Calculator",
+        category: "Materials",
+        description: "Coming Soon",
+        link: "#"
+    }
+];
